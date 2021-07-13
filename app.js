707fcd8c762c00c9dedf820af74df77878932f2a
teagang <42443593+teagang@users.noreply.github.com>
2021-07-13 13:27:00 +0200

    Form basic structure of website including nav with various pages

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const path = require('path');
+const ejsMate = require('ejs-mate');
+
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.render('pages/landing')
+});
+
+app.get('/about', (req, res) => {
+    res.render('pages/about');
+});
+
+app.get('/skills', (req, res) => {
+    res.render('pages/skills');
+});
+
+app.get('/gallery', (req, res) => {
+    res.render('pages/gallery');
+});
+
+app.get('/contact', (req, res) => {
+    res.render('pages/contact');
+});
+
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Serving on port ${port}`)
+})
